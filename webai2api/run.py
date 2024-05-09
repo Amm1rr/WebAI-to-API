6acccffa9ed91b11bdedc3dde70e54a7c4cffae2
Amir <soheyl637@gmai.com>
2024-05-09 05:59:59 +0100

    Update logging configuration in run.py for utility module

--- webai2api/run.py
+++ webai2api/run.py
@@ -1,14 +1,16 @@
 from webai2api.__main__ import app
+from webai2api.utils import utility
 import logging
 
-logging.basicConfig(level=logging.INFO)
-# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+utility.configure_logging()
 logging.info("run.py")
+
 
 def run():
     import uvicorn
     logging.info("run.py.run()")
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
 if __name__ == "__main__":
     logging.info("run.py.name()")
