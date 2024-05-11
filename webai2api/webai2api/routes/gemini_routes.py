89645e16e8fe7dbdf69350b4aaa1529766424da0
Amir <soheyl637@gmai.com>
2024-05-11 06:08:09 +0100

    refactor(gemini_routes): Reorder imports, remove unused parameters and clean up comments

--- webai2api/webai2api/routes/gemini_routes.py
+++ webai2api/webai2api/routes/gemini_routes.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Request
-from gemini_webapi import GeminiClient
-from ..utils import utility
-import logging
 import copy
 import json
+import logging
+
+from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
+from gemini_webapi import GeminiClient
+
+from ..utils import utility
 
 utility.configure_logging()
 logging.info("gemini_routes.py")
@@ -14,9 +16,9 @@ router = APIRouter()
 COOKIE_GEMINI = None
 GEMINI_CLIENT = None
 
+
 async def initialize_gemini():
     logging.info("gemini_routes.py./startup_event")
-    import os
     global COOKIE_GEMINI, GEMINI_CLIENT
     COOKIE_GEMINI = utility.getCookie_Gemini()
     GEMINI_CLIENT = GeminiClient()
@@ -25,16 +27,17 @@ async def initialize_gemini():
     except Exception as e:
         print(e)
 
+
 @router.on_event("startup")
 async def startup_event():
     await initialize_gemini()
 
+
 @router.post("/gemini")
-async def ask_gemini(request: Request, message: dict):
+async def ask_gemini(message: dict):
     """API endpoint to get response from Google Gemini.
 
     Args:
-        request (Request): API request object
         message (Message): Message request object
 
     Returns:
@@ -48,30 +51,30 @@ async def ask_gemini(request: Request, message: dict):
 
     """
     logging.info("gemini_routes.py./gemini")
-    
+
     conversation_id = message.get('conversation_id')
     if conversation_id == "string":
         message['conversation_id'] = None
         conversation_id = None
-    
+
     original_conversation_id = copy.deepcopy(conversation_id)
-    
-    stream = message.get('stream', False)
 
     prompt = message.get('message', "What is your name?")
-    
+
     if not GEMINI_CLIENT:
-        return {"warning": "Looks like you're not logged in to Gemini. Please either set the Gemini cookie manually or log in to your gemini.google.com account through your web browser."}
+        return {
+            "warning": "Looks like you're not logged in to Gemini. Please either set the Gemini cookie manually or "
+                       "log in to your gemini.google.com account through your web browser."}
 
     try:
         response = await GEMINI_CLIENT.generate_content(prompt=prompt)
-        
+
         json_data = response.json()
 
         # Load the JSON data
         data = json.loads(json_data)
 
-        ## Accessing elements:
+        # Accessing elements:
         # metadata = data["metadata"] 
         candidates = data["candidates"]
         # chosen_index = data["chosen"]
@@ -79,23 +82,21 @@ async def ask_gemini(request: Request, message: dict):
         # Extract specific information from the first candidate
         # first_candidate_rcid = candidates[0]["rcid"]
         first_candidate_text = candidates[0]["text"]
-        
+
         # print(first_candidate_text)
         # return first_candidate_text
         return StreamingResponse(
-                    first_candidate_text,
-                    media_type="text/event-stream",
-                )
-    
+            first_candidate_text,
+            media_type="text/event-stream",
+        )
+
     except Exception as req_err:
         print(f"Error Occurred: {req_err}")
         return f"Error Occurred: {req_err}"
 
-# @router.post("/gemini")
-# async def ask_gemini(request: Request, message: dict):
-#     logging.info("gemini_routes.py./gemini")
-#     if not GEMINI_CLIENT:
-#         yield {"warning": "Looks like you're not logged in to Gemini. Please either set the Gemini cookie manually or log in to your gemini.google.com account through your web browser."}
+# @router.post("/gemini") async def ask_gemini(request: Request, message: dict): logging.info(
+# "gemini_routes.py./gemini") if not GEMINI_CLIENT: yield {"warning": "Looks like you're not logged in to Gemini.
+# Please either set the Gemini cookie manually or log in to your gemini.google.com account through your web browser."}
 
 #     conversation_id = message.get('conversation_id')
 #     if conversation_id == "string":
@@ -111,5 +112,5 @@ async def ask_gemini(request: Request, message: dict):
 #     except Exception as req_err:
 #         print(f"Error Occurred: {req_err}")
 #         yield f"Error Occurred: {req_err}"
-    
+
 #     return
